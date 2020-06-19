71f141c66a116143ca64bbca914f8e8805ac9158
AllenThomasDev <allen.thomas.dev@gmail.com>
2020-06-19 17:29:48 +0530

    Procfile and .gitignore added

---
+++ songs.py
@@ -0,0 +1,12 @@
+songs=[    {'label':'Queen - We Will Rock You','value': 'https://www.metrolyrics.com/we-will-rock-you-lyrics-queen.html'},
+    {'label':'Adele - Rumour has it','value': 'https://www.metrolyrics.com/rumour-has-it-lyrics-adele.html'},
+    {'label':'Drake - Hotline Bling','value': 'https://www.metrolyrics.com/hotline-bling-lyrics-drake.html'},
+    {'label':'Rick Astley - Never Gonna Give you up','value': 'https://www.metrolyrics.com/never-gonna-give-you-up-lyrics-rick-astley.html'},
+    {'label':'Beyonce - Formation','value': 'https://www.metrolyrics.com/formation-lyrics-beyonce.html'},
+    {'label':'The Weeknd - Starboy','value': 'https://www.metrolyrics.com/starboy-lyrics-the-weeknd.html'},
+    {'label':'Vince Staples - Yeah Right','value': 'https://www.metrolyrics.com/yeah-right-lyrics-vince-staples.html'},
+    {'label':'Lil Nas X - Old Town Road','value': 'https://www.metrolyrics.com/old-town-road-lyrics-lil-nas-x.html'},
+    {'label':'Luis Fonsi - Despacito ft. Daddy Yankee','value': 'https://www.metrolyrics.com/despacito-lyrics-luis-fonsi.html'},
+    {'label':'Toto - Africa','value': 'https://www.metrolyrics.com/africa-lyrics-toto.html'},
+    {'label':'Frank Ocean - Cayendo','value':'https://www.metrolyrics.com/cayendo-acoustic-lyrics-frank-ocean.html'}
+    ]
